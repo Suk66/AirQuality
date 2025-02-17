6811560d6b57dd9b21dbb74216eaae0d63591348
Suk66 <goawwl66@gmail.com>
2025-02-17 16:47:45 +0900

    테스트1

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # 미세먼지 스프링부트앱
 
 ### 에어코리아 미세먼지 측정 api
-+ https://apis.data.go.kr/B552584/ArpltnInforInqireSvc/getCtprvnRltmMesureDnsty?serviceKey=l6cIX%2Fd6ZbPSsGi9Ps%2F9xYQT7rdgvty0%2BLE7ccAs8gsob%2BHAeqltGv4ObCgsz9csfPQxzocVLt5251M1RPguUA%3D%3D&returnTyphttps://apis.data.go.kr/B552584/ArpltnInforInqireSvc/getCtprvnRltmMesureDnsty?serviceKey=&returnType=json&numOfRows=100&pageNo=1&sidoName=%EC%84%9C%EC%9A%B8&ver=1.0
++ https://apis.data.go.kr/B552584/ArpltnInforInqireSvc/getCtprvnRltmMesureDnsty?serviceKey=&returnType=json&numOfRows=100&pageNo=1&sidoName=%EC%84%9C%EC%9A%B8&ver=1.0
--- src/main/java/com/example/goawwl66/airquality/controller/AirController.java
+++ src/main/java/com/example/goawwl66/airquality/controller/AirController.java
@@ -1,15 +1,15 @@
-package com.example.goawwl66.airquality.controller;
-
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-
-@Controller
-public class AirController {
-    @GetMapping("/air")
-    niceToMeetYou(Model model) {
-        model.addAttribute("air", "서울시 미세먼지");
-        retrun "";
-    }
-
-}
+//compackage com.example.goawwl66.airquality.controller;
+//
+//import org.springframework.stereotype.Controller;
+//import org.springframework.ui.Model;
+//import org.springframework.web.bind.annotation.GetMapping;
+//
+//@Controller
+//public class AirController {
+//    @GetMapping("/air")
+//    niceToMeetYou(Model model) {
+//        model.addAttribute("air", "서울시 미세먼지");
+//        retrun "";
+//    }
+//
+//}///////
